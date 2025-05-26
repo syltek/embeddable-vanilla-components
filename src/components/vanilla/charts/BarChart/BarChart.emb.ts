0e4e65d6d9ada0a978e3feae54c1568785fa8f76
Christopher Buecheler <cwbuecheler@users.noreply.github.com>
2025-05-26 10:38:47 -0400

    CS-65 - Bar Chart Sorting (#49)
    * Allow sorting by any dimension/measure on bar chart
    
    * remove unnecessary else if

--- src/components/vanilla/charts/BarChart/BarChart.emb.ts
+++ src/components/vanilla/charts/BarChart/BarChart.emb.ts
@@ -1,4 +1,11 @@
-import { OrderBy, loadData } from '@embeddable.com/core';
+import {
+  isDimension,
+  isMeasure,
+  OrderBy,
+  loadData,
+  Measure,
+  Dimension,
+} from '@embeddable.com/core';
 import { EmbeddedComponentMeta, Inputs, defineComponent } from '@embeddable.com/react';
 import Component from './index';
 
@@ -174,14 +181,24 @@ export default defineComponent(Component, meta, {
       });
     }
 
+    // Allow sorting by any dimension or measure regardless of x-axis
+    const dimensions = [inputs.xAxis];
+    const measures = [...inputs.metrics, ...(inputs.lineMetrics || [])];
+    if (inputs.sortBy && isDimension(inputs.sortBy) && !dimensions.includes(inputs.sortBy)) {
+      dimensions.push(inputs.sortBy as Dimension);
+    }
+    if (inputs.sortBy && isMeasure(inputs.sortBy) && !measures.includes(inputs.sortBy)) {
+      measures.push(inputs.sortBy as Measure);
+    }
+
     return {
       ...inputs,
       clientContext,
       reverseXAxis: inputs.reverseXAxis,
       results: loadData({
         from: inputs.ds,
-        dimensions: [inputs.xAxis],
-        measures: [...inputs.metrics, ...(inputs.lineMetrics || [])],
+        dimensions,
+        measures,
         orderBy: orderProp,
         limit: inputs.limit || 50,
       }),
